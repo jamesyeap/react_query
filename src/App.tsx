1c240ca9dd5b70390abbdb4fae91aec85f9be333
James Yeap <jamesyeap.ys@gmail.com>
2021-12-28 17:22:45 +0800

    Install React Query DevTools

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 import axios from "axios";
 import React from "react";
 import { useQuery } from 'react-query';
+import { resourceLimits } from "worker_threads";
 
-const fetchAllPokemon = () => {
+const fetchAllPokemon = async () => {
+  // simulate loading time cuz internet connection too fast
+  //    to actually see the loading screen
+  await new Promise(resolve => setTimeout(resolve, 1000));
+
+  // simulate error being thrown during request
+  if (true) { throw new Error('Test error'); }
+
   return axios.get("https://pokeapi.co/api/v2/pokemon")
-              .then(res => console.log(res));
+              .then(res => res.data.results);
 }
 
 function App() {
-  const queryInfo = useQuery('pokemon', fetchAllPokemon);
+  const queryInfo:any = useQuery('pokemon', fetchAllPokemon);
 
-  return (<div>Hello.</div>);
+  if (queryInfo.isLoading) { return (<div>Loading...</div>) }
+  if (queryInfo.isError) { return (<div>{queryInfo.error.message}</div>) }
+
+  return (
+    <div>
+      {queryInfo.data?.map((elem:any) => (
+        <div key={elem.name}>{elem.name}</div>
+      ))}
+    </div>
+  )
 }
 
 export default App;
